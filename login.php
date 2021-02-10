96bff684ed032891e145f48b0ca949b95d2a1c4a
Bieldeandrade <69852854+Bieldeandrade@users.noreply.github.com>
2021-02-10 09:34:24 -0300

    Nova aula

--- login.php
+++ login.php
@@ -1,48 +1,65 @@
-<?php
-include'connect.php';
-if(isset($_POST['sub'])){
-    $u=$_POST['user'];
-    $p=$_POST['pass'];
-    $s= "select * from reg where username='$u' and password= '$p'";   
-   $qu= mysqli_query($con, $s);
-   if(mysqli_num_rows($qu)>0){
-      $f= mysqli_fetch_assoc($qu);
-      $_SESSION['id']=$f['id'];
-      header ('location:home.php');
+<? php
+
+// Conexão com banco de dados 
+incluem 'connect.php' ;
+
+if ( isset ( $ _POST [ 'sub' ])) {
+    $ u = $ _POST [ 'usuário' ];
+    $ p = $ _POST [ 'passagem' ];
+    $ s = "selecione * de reg onde nome de usuário = '$ u' e senha = '$ p'" ;   
+   $ qu = mysqli_query ( $ con , $ s );
+   if ( mysqli_num_rows ( $ qu )> 0 ) {
+      $ f = mysqli_fetch_assoc ( $ qu );
+      $ _SESSION [ 'id' ] = $ f [ 'id' ];
+      cabeçalho ( 'localização: home.php' );
    }
-   else{
-       echo 'username or password does not exist';
+   else {
+       echo  'nome de usuário ou senha não existe' ;
    }
   
 }
 ?>
-<html>
+< html >
       
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <form method="POST" enctype="multipart/form-data">
-            <table>
+    < cabeça >
+        < meta  charset = " UTF-8 " >
+        < título > </ título >
+    </ head >
+    < corpo >
+        < form  method = " POST " enctype = " multipart / form-data " >
+            < mesa >
                 
-                <tr>
-                    <td>
-                        Username
-                        <input type="text" name="user">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        password
-                        <input type="password" name="pass">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="submit" name="sub" value="submit">
-                    </td>
-                </tr>
-            </table>
-    </body>
-</html>
+                < tr >
+                    < td >
+                        Nome do usuário
+                        < input  type = " text " name = " user " >
+                    </ td >
+                </ tr >
+                < tr >
+                    < td >
+                        senha
+                        < input  type = " senha " name = " pass " >
+                    </ td >
+                </ tr >
+                < tr >
+                    < td >
+                        < input  type = " submit " name = " sub " value = " submit " >
+                    </ td >
+                </ tr >
+
+                < tr >
+                    < td >
+                        < input  type = " submit " value = " Register " name = " reg " >
+                               
+                    </ td >
+                </ tr >
+
+                < tr >
+                    < td >
+                        < A  href = " reg.php " > Registrar </ a >
+                    </ td >
+                </ tr >
+                
+            </ mesa >
+    </ body >
+</ html >
--- reg.php
+++ reg.php
@@ -1,74 +1,82 @@
-<?php
-include 'connect.php';
-if(isset($_POST['sub'])){
-    $t=$_POST['text'];
-    $u=$_POST['user'];
-    $p=$_POST['pass'];
-    $c=$_POST['city'];
-    $g=$_POST['gen'];
-    if($_FILES['f1']['name']){
-    move_uploaded_file($_FILES['f1']['tmp_name'], "image/".$_FILES['f1']['name']);
-    $img="image/".$_FILES['f1']['name'];
+<? php
+incluem  'connect.php' ;
+if ( isset ( $ _POST [ 'sub' ])) {
+    $ t = $ _POST [ 'texto' ];
+    $ u = $ _POST [ 'usuário' ];
+    $ p = $ _POST [ 'passagem' ];
+    $ c = $ _POST [ 'cidade' ];
+    $ g = $ _POST [ 'gen' ];
+    if ( $ _FILES [ 'f1' ] [ 'nome' ]) {
+    move_uploaded_file ( $ _FILES [ 'f1' ] [ 'tmp_name' ], "imagem /" . $ _FILES [ 'f1' ] [ 'nome' ]);
+    $ img = "imagem /" . $ _FILES [ 'f1' ] [ 'nome' ];
     }
-    $i="insert into reg(name,username,password,city,image,gender)value('$t','$u','$p','$c','$img','$g')";
-    mysqli_query($con, $i);
+    $ i = "inserir em reg (nome, nome de usuário, senha, cidade, imagem, gênero) valor ('$ t', '$ u', '$ p', '$ c', '$ img', '$ g ') " ;
+    mysqli_query ( $ con , $ i );
 }
 ?>
 
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <form method="POST" enctype="multipart/form-data">
-            <table>
-                <tr>
-                    <td>
-                        Name
-                        <input type="text" name="text">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Username
-                        <input type="text" name="user">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        password
-                        <input type="password" name="pass">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        city
-                        <select name="city">
-                            <option value="">-select-</option>
-                            <option value="knp">kanpur</option>
-                            <option value="lko">lucknow</option>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Gender
-                        <input type="radio"name="gen" id="gen" value="male">male
-                        <input type="radio" name="gen" id="gen" value="female">female
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Image
-                        <input type="file" name="f1">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="submit" value="submit" name="sub">
+< html >
+    < cabeça >
+        < meta  charset = " UTF-8 " >
+        < título > </ título >
+    </ head >
+    < corpo >
+        < form  method = " POST " enctype = " multipart / form-data " >
+            < mesa >
+                < tr >
+                    < td >
+                        Nome
+                        < input  type = " text " name = " text " >
+                    </ td >
+                </ tr >
+                < tr >
+                    < td >
+                        Nome do usuário
+                        < input  type = " text " name = " user " >
+                    </ td >
+                </ tr >
+                < tr >
+                    < td >
+                        senha
+                        < input  type = " senha " name = " pass " >
+                    </ td >
+                </ tr >
+                < tr >
+                    < td >
+                        cidade
+                        < selecionar  nome = " cidade " >
+                            < option  value = "" > -selecionar- </ option >
+                            < option  value = " knp " > kanpur </ option >
+                            < option  value = " lko " > lucknow </ option >
+                    </ td >
+                </ tr >
+                < tr >
+                    < td >
+                        Gênero
+                        < input  type = " radio " name = " gen " id = " gen " value = " male " > masculino
+                        < input  type = " radio " name = " gen " id = " gen " value = " female " > feminino
+                    </ td >
+                </ tr >
+                < tr >
+                    < td >
+                        Imagem
+                        < input  type = " file " name = " f1 " >
+                    </ td >
+                </ tr >
+                < tr >
+                    < td >
+                        < input  type = " submit " value = " submit " name = " sub " >
                                
-                    </td>
-                </tr>
-            </table>
-    </body>
-</html>
+                    </ td >
+                </ tr >
+
+
+
+                < tr >
+                    < td >
+                        < A  href = " login.php " > Acesso </ a >
+                    </ td >
+                </ tr >
+            </ mesa >
+    </ body >
+</ html >
